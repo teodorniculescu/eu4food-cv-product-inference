ef864861658429b23ace348ee148051208280979
teodorniculescu <teodorvicentiuniculescu@gmail.com>
2023-04-03 12:28:39 +0300

    Modified setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install software-properties-common
+sudo apt -y install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.8
-sudo apt install python3.8-venv
+sudo apt -y install python3.8
+sudo apt -y install python3.8-venv
 
-python3.8 -m venv my_virtual_environment/bin/activate
-source my_virtual_environment/bin/activate
-pip3 install --upgrade pip
-pip3 install --upgrade setuptools
-pip3 install opencv-contrib-python==4.1.2.30 opencv-python==4.1.2.30 scikit-learn
-pip3 install torch torchvision torchaudio
-pip3 install tqdm
+$env_name=eu4food_cv_product_inference_venv 
+
+python3.8 -m venv $env_name/bin/activate
+source $env_name/bin/activate
+pip3 install -y --upgrade pip
+pip3 install -y --upgrade setuptools
+pip3 install -y opencv-contrib-python==4.1.2.30 opencv-python==4.1.2.30 scikit-learn
+pip3 install -y torch torchvision torchaudio
+pip3 install -y tqdm
