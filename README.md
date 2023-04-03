72a2d38175fac0ce0d26cb2ccba962fbdb58dbee
teodorniculescu <teodorvicentiuniculescu@gmail.com>
2023-04-03 12:44:48 +0300

    Update readme and setup

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# eu4food-cv-product-inference
+Setup:
+
+1. Clone repository:
+git clone https://github.com/teodorniculescu/eu4food-cv-product-inference.git
+
+2. Go to the newly created folder and run the setup.sh script
+cd eu4food-cv-product-inference
+chmod 777 setup.sh
+./setup.sh
+
+Note: Accept prompts regarding package installation if necessary.
+
--- setup.sh
+++ setup.sh
@@ -5,11 +5,11 @@ sudo apt -y install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt -y install python3.8 python3.8-venv
 
-$env_name=eu4food_cv_product_inference_venv 
+env_name=eu4food_cv_product_inference_venv 
 
-python3.8 -m venv $env_name/bin/activate
+python3.8 -m venv $env_name
 source $env_name/bin/activate
-pip3 install -y --upgrade pip setuptools
-pip3 install -y opencv-contrib-python==4.1.2.30 opencv-python==4.1.2.30 scikit-learn
-pip3 install -y torch torchvision torchaudio
-pip3 install -y tqdm
+pip3 install --upgrade pip setuptools
+pip3 install opencv-contrib-python==4.1.2.30 opencv-python==4.1.2.30 scikit-learn
+pip3 install torch torchvision torchaudio
+pip3 install tqdm
