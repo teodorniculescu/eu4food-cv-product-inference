2ed25c72d1428fcc3fa8cf4d64238bf49778ae52
teodorniculescu <teodorvicentiuniculescu@gmail.com>
2023-04-06 00:28:38 +0300

    modified train script

--- train.sh
+++ train.sh
@@ -2,11 +2,12 @@
 
 save_path=cpy_train_results
 
-: << COMMENT
-BUCKET_NAME=gs://eu4food-dataset
+DATASET_BUCKET_NAME=gs://eu4food-dataset
+MODEL_BUCKET_NAME=gs://eu4food-public
 DATASET_PATH=eu4food-dataset/Images
 ENV_NAME=eu4food_cv_product_inference_venv 
 
+: << COMMENT
 model=resnet18
 batch_size=64
 learning_rate=0.01
@@ -18,13 +19,13 @@ num_epochs=2
 num_workers=3
 
 # Download files from the bucket
-gsutil -m cp -r $BUCKET_NAME .
+gsutil -m cp -r $DATASET_BUCKET_NAME .
 
 # Check the exit status of the gsutil command
 if [ $? -eq 0 ]; then
-	echo "Downloaded files from $BUCKET_NAME"
+	echo "Downloaded files from $DATASET_BUCKET_NAME"
 else
-	echo "ERROR: Could not download files from $BUCKET_NAME"
+	echo "ERROR: Could not download files from $DATASET_BUCKET_NAME"
 	exit 1
 fi
 
@@ -54,7 +55,7 @@ rm -rf $DATASET_PATH
 
 COMMENT
 
-most_recent_train=$(ls -v1 $save_path | tail -n 1)
+most_recent_train=$save_path/$(ls -v1 $save_path | tail -n 1)
 if [ $? -eq 0 ]; then
 	echo "Most recent train is $most_recent_train"
 else
@@ -62,3 +63,52 @@ else
 	exit 1
 fi
 
+gsutil ls $MODEL_BUCKET_NAME >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+	echo "Bucket $MODEL_BUCKET_NAME exists"
+else
+	echo "ERROR: No such bucket $MODEL_BUCKET_NAME"
+	exit 1
+fi
+
+upload=1
+
+gsutil ls $MODEL_BUCKET_NAME/best_model/
+if [ $? -eq 1 ]; then
+	echo "First model being uploaded"
+	upload=0
+
+else
+	echo "Compare current train performance with uploaded model performance"
+
+	gsutil -m cp -r $MODEL_BUCKET_NAME/best_model/scores.json uploaded_model_scores.json
+
+	if [ $? -eq 0 ]; then
+		echo "Copied scores"
+	else
+		echo "ERROR: Could not copy scores"
+		exit 1
+	fi
+
+	uploaded_f1=$(cat uploaded_model_scores.json | jq -r '.test.f1')
+	local_f1=$(cat $most_recent_train/scores.json | jq -r '.test.f1')
+
+	if (( $(echo "$local_f1 > $uploaded_f1" | bc -l) )); then
+		echo "Local F1 is greater than uploaded F1"
+		upload=0
+	else
+		echo "Local F1 is not greater than uploaded F1. No need to upload files."
+		upload=1
+	fi
+
+fi
+
+if [ $upload -eq 0 ]; then
+	gsutil -m cp -r $most_recent_train $MODEL_BUCKET_NAME
+	if [ $? -eq 0 ]; then
+		echo "Successfuly uploaded new model"
+	else
+		echo "ERROR: Could not upload model"
+		exit 1
+	fi
+fi
